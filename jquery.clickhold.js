a17eec0bc2411caba8f46a16514d2a0b3a08432f
piyopoco <taiichi.works@gmail.com>
2013-02-20 17:59:33 +0900

    Update jquery.clickhold.js
    ホールド判定の時間とホールド中の実行間隔を別々に設定できるようにしたよ。

--- jquery.clickhold.js
+++ jquery.clickhold.js
@@ -2,21 +2,43 @@
 	
 	$.fn.clickHold = function( callback, completeCallback ) {
 		
+		var holdTime = 50;
+		var intervalExeTime = 10;
+
+		var iniTimer = [];
+		var count = 0;
 		var intervalTimer;
 		var active;
+
+		var clearIniInterval = function(){
+			$.each(iniTimer, function(i){
+				clearInterval(iniTimer[i]);
+			});
+			iniTimer=[];
+			count = 0;
+		}
 		
 		return this.mousedown(function(){
-			active = true;
-			callback();
-			intervalTimer = setInterval(function(){ callback(); }, 100);
+			
+			clearIniInterval();
+
+			var isiid = setInterval(function(){
+				if(count > holdTime){
+					active = true;
+					intervalTimer = setInterval(function(){ callback(); }, intervalExeTime);
+				}
+				count++;
+			}, 1);
+			iniTimer.push(isiid);
+			
 		}).bind('mouseup mouseout', function(){
-			if(active)
-			{
+
+			clearIniInterval();
+
+			if(active){
 				active = false;
 				clearInterval(intervalTimer);
-				
-				if(typeof completeCallback !== 'undefined')
-				{
+				if(typeof completeCallback !== 'undefined'){
 					completeCallback();
 				}
 			}
